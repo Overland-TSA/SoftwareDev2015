4e1492d62ca5cf1e5ddf3a2e6fb27bec33eb0581
jmuller4 <jmuller4@IST-IE103-007.ccsd.d5>
2015-01-05 12:19:01 -0700

    yay

--- OverlandApp/gen/org/overland_tsa/overlandapp/R.java
+++ OverlandApp/gen/org/overland_tsa/overlandapp/R.java
@@ -94,10 +94,46 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int announcements=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int ceja_email=0x7f05001e;
+        public static final int ceja_name=0x7f050012;
+        public static final int ceja_phone=0x7f05002a;
+        public static final int clark_email=0x7f05001d;
+        public static final int clark_name=0x7f050011;
+        public static final int clark_phone=0x7f050029;
+        public static final int fredericks_email=0x7f050019;
+        public static final int fredericks_name=0x7f05000d;
+        public static final int fredericks_phone=0x7f050025;
+        public static final int fritz_email=0x7f050013;
+        public static final int fritz_name=0x7f050007;
+        public static final int fritz_phone=0x7f05001f;
+        public static final int grandchamp_email=0x7f050015;
+        public static final int grandchamp_name=0x7f050009;
+        public static final int grandchamp_phone=0x7f050021;
+        public static final int houck_email=0x7f050017;
+        public static final int houck_name=0x7f05000b;
+        public static final int houck_phone=0x7f050023;
+        public static final int hughes_email=0x7f05001b;
+        public static final int hughes_name=0x7f05000f;
+        public static final int hughes_phone=0x7f050027;
+        public static final int lorenzo_email=0x7f050014;
+        public static final int lorenzo_name=0x7f050008;
+        public static final int lorenzo_phone=0x7f050020;
+        public static final int otis_email=0x7f050016;
+        public static final int otis_name=0x7f05000a;
+        public static final int otis_phone=0x7f050022;
         public static final int overlandWeb=0x7f050002;
+        public static final int pineda_email=0x7f050018;
+        public static final int pineda_name=0x7f05000c;
+        public static final int pineda_phone=0x7f050024;
+        public static final int szenderski_email=0x7f05001a;
+        public static final int szenderski_name=0x7f05000e;
+        public static final int szenderski_phone=0x7f050026;
         public static final int url_athletics=0x7f050006;
         public static final int url_overland=0x7f050004;
         public static final int url_powerschool=0x7f050005;
+        public static final int williams_email=0x7f05001c;
+        public static final int williams_name=0x7f050010;
+        public static final int williams_phone=0x7f050028;
     }
     public static final class style {
         /** 
